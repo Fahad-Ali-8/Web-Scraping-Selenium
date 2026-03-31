370701cb59090c9c60ef8787a5291a4578216f68
Fahad Ali <fahadali45621@gmail.com>
2026-03-31 17:46:43 +0500

    clicking buttons

--- main.py
+++ main.py
@@ -24,12 +24,14 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+import time
 driver = webdriver.Chrome()
 
-driver.get("https://www.timesjobs.com")
+driver.get("https://the-internet.herokuapp.com/?utm_source=chatgpt.com")
 
-title = driver.find_element(By.TAG_NAME, "h1").text
-print(title)
+element = driver.find_element(By.CSS_SELECTOR, 'a[href="/checkboxes"]')
+element.click() 
+# title = driver.find_element(By.TAG_NAME, "h1").text
+# print(title)
 
-driver.quit()
+time.sleep(10)
