9115f985fbe7a5cf1f650898bb3e3a173f182f27
Fahad Ali <fahadali45621@gmail.com>
2026-04-01 17:25:33 +0500

    Learning Xpath

--- Task_2.py
+++ Task_2.py
@@ -3,4 +3,23 @@ from selenium.webdriver.common.by import By
 import time
 
 
-driver.get("https://the-internet.herokuapp.com/?utm_source=chatgpt.com")
+driver = webdriver.Chrome()
+driver.get("https://the-internet.herokuapp.com/login")
+element = driver.find_element(By.XPATH, "//input[@name='username']")
+element.click()
+element.send_keys("tomsmith")
+
+element_2 = driver.find_element(By.XPATH, "//input[@name='password']")
+element_2.click()
+element_2.send_keys("SuperSecretPassword!")
+
+element_3 = driver.find_element(By.XPATH, "//button")
+element_3.click()
+
+
+element_4 = driver.find_element(By.XPATH, "//div[@class='flash success']")
+print(element_4.text)
+
+
+
+time.sleep(3)
