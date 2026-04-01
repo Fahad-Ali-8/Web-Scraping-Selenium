7e4a9a27329e70c9f1425c6c3ca32191f479b33b
Fahad Ali <fahadali45621@gmail.com>
2026-04-01 08:48:47 +0500

    Working with checkboxes

--- Task_1.py
+++ Task_1.py
@@ -9,13 +9,27 @@ import time
 # print(title)
 # time.sleep(5)
 
+
 # TASK 2
+# driver = webdriver.Chrome()
+# driver.get("https://the-internet.herokuapp.com/?utm_source=chatgpt.com")
+# element = driver.find_element(By.CSS_SELECTOR, "a[href='/checkboxes']")
+# element.click()
+# element_2 = driver.find_elements(By.CSS_SELECTOR, "#checkboxes input")
+# element_2[0].click()
+# print(element_2[0].is_selected())
+# time.sleep(3)
+
+# TASK 3
 driver = webdriver.Chrome()
 driver.get("https://the-internet.herokuapp.com/?utm_source=chatgpt.com")
-element = driver.find_element(By.CSS_SELECTOR, "a[href='/checkboxes']")
+
+element = driver.find_element(By.CSS_SELECTOR, "a[href='/inputs']")
 element.click()
 
-element_2 = driver.find_elements(By.CSS_SELECTOR, "#checkboxes input")
-element_2[0].click()
-print(element_2[0].is_selected())
+element_2 = driver.find_element(By.CSS_SELECTOR, "input[type='number']")
+element_2.click()
+element_2.send_keys("12345")
+element_2.clear()
+element_2.send_keys("999")
 time.sleep(3)
---
+++ Task_2.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+
+driver.get("https://the-internet.herokuapp.com/?utm_source=chatgpt.com")
