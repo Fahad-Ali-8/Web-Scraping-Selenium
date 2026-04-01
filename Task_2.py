56f956bb34713991ee173b7b3dc4587e883eb44f
Fahad Ali <fahadali45621@gmail.com>
2026-04-01 20:15:43 +0500

    Worked with loops and verification with len function

--- Task_2.py
+++ Task_2.py
@@ -3,23 +3,41 @@ from selenium.webdriver.common.by import By
 import time
 
 
+# driver = webdriver.Chrome()
+# driver.get("https://the-internet.herokuapp.com/login")
+# element = driver.find_element(By.XPATH, "//input[@name='username']")
+# element.click()
+# element.send_keys("tomsmith")
+
+# element_2 = driver.find_element(By.XPATH, "//input[@name='password']")
+# element_2.click()
+# element_2.send_keys("SuperSecretPassword!")
+
+# element_3 = driver.find_element(By.XPATH, "//button")
+# element_3.click()
+
+# element_4 = driver.find_element(By.XPATH, "//div[@class='flash success']")
+# print(element_4.text)
+
+# time.sleep(3)
+
+
+
+#TASK 2
+
+
 driver = webdriver.Chrome()
-driver.get("https://the-internet.herokuapp.com/login")
-element = driver.find_element(By.XPATH, "//input[@name='username']")
-element.click()
-element.send_keys("tomsmith")
+driver.get("https://the-internet.herokuapp.com/add_remove_elements/?utm_source=chatgpt.com")
+element = driver.find_element(By.XPATH, "//button[normalize-space()='Add Element']")
+for i in range(3):
+    element.click() 
 
-element_2 = driver.find_element(By.XPATH, "//input[@name='password']")
-element_2.click()
-element_2.send_keys("SuperSecretPassword!")
-
-element_3 = driver.find_element(By.XPATH, "//button")
-element_3.click()
-
-
-element_4 = driver.find_element(By.XPATH, "//div[@class='flash success']")
-print(element_4.text)
-
-
-
-time.sleep(3)
+buttons = driver.find_elements(By.XPATH, "//button[@class='added-manually']")
+for btn in buttons:
+    btn.click()
+buttons = driver.find_elements(By.XPATH, "//button[@class='added-manually']")
+if len(buttons) == 0:
+    print("Worked")
+else:
+    print("SOmething is wrong")    
+time.sleep(5)
